bb2ecfc7c0b294eeb1d9ccd7c103ed26d4d747f4
Colin Marc <colinmarc@gmail.com>
2016-03-05 00:12:55 +0100

    Test the slackbot in jenkins_build

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -5,7 +5,8 @@ set -eux
 cd /go/src/github.com/stripe/timberlake
 make
 
-./timberlake -help 2>&1 | grep Usage && echo 'binary looks good'
+bin/timberlake -help 2>&1 | grep Usage && echo 'binary looks good'
+bin/timberlake-slackbot -help 2>&1 | grep Usage && echo 'binary looks good'
 
 mkdir -p /build
 cp -r bin static index.html /build/
