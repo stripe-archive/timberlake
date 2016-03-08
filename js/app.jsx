fff7c84dd2e01cc8099287c61a7d880ba015e464
Betsy Cannon <betsy@stripe.com>
2016-03-08 10:20:00 -0800

    Tighten whitespace in kill confirmation modal

--- js/app.jsx
+++ js/app.jsx
@@ -395,7 +395,7 @@ var KillModal = React.createClass({
     var killJob = this.props.killJob;
     return (
       <div className="modal show" tabIndex="-1" role="dialog">
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-kill">
           <div className="modal-content">
             <div className="modal-body">
               <h4>Are you sure you want to kill this job?</h4>
