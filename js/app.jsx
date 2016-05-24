54fad0829e53f51f4b23f00b9d8d4817fee487c8
Andrew Bullen <abull@stripe.com>
2016-05-24 15:04:17 -0700

    Redirect to no-trailing-slash versions of urls rather than having an optional trailing slash

--- js/app.jsx
+++ js/app.jsx
@@ -860,9 +860,10 @@ var ReduceSummary = React.createClass({
 React.render(
   <Routes>
     <Route handler={App}>
+      <ReactRouter.Redirect from="/*/" to="/*" />
       <Route name="app" path="/" handler={BigData} ignoreScrollBehavior={true} />
-      <Route name="job" path="/job/:jobId/?" handler={Job} />
-      <Route name="logs" path="/job/:jobId/logs/?" handler={JobLogs} />
+      <Route name="job" path="/job/:jobId" handler={Job} />
+      <Route name="logs" path="/job/:jobId/logs" handler={JobLogs} />
     </Route>
   </Routes>
 , document.body);
